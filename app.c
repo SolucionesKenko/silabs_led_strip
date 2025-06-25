014613a9391370dc975c4d1295a4b45319fce27c
Soluciones Kenko <116828620+SnaiderBG-SK@users.noreply.github.com>
2025-06-25 17:55:00 -0600

    Update app.c

--- app.c
+++ app.c
@@ -29,17 +29,76 @@
 
 neopixel_t strips;
 
+void delay_ms(uint32_t ms)
+{
+  // for (uint32_t i = 0; i < 1000000; i++)
+  //   {
+  //     __NOP();
+  //     __NOP();
+  //     __NOP();
+  //     __NOP();
+  //     __NOP();
+  //     __NOP();
+  //     __NOP();
+  //   }
+  sl_sleeptimer_delay_millisecond(64);
+}
+
+static void dma_transfer_syn()
+{
+//  delay_ms(10);
+  neopixel_update(&strips, TIMER_TopGet(TIMER0));
+//  delay_ms(10);
+  neopixel_init_dma_transfer(&strips, (void*)&TIMER0->CC[0].OCB);
+}
+uint8_t led_state = 0;
+static void patern_0(void) {
+    for (int i = 0; i < 8; i++)
+    {
+       sl_sleeptimer_delay_millisecond(64);
+       neopixel_set_all(&strips, NEOPIXEL_STRIP_ID_0, 0, 0, 255, 0);
+       dma_transfer_syn();
+       sl_sleeptimer_delay_millisecond(64);
+       neopixel_set_all(&strips, NEOPIXEL_STRIP_ID_0, 0, 0, 0, 0);
+//       while(LDMA_TransferDone(DMA_CONFIG_DMA_CHANNEL) != 1)
+//              {
+//
+//              }
+       dma_transfer_syn();
+    }
+}
+
+static void patern_1(void) {
+    for (int i = 0; i < 8; i++)
+    {
+        sl_sleeptimer_delay_millisecond(64);
+        neopixel_set_all(&strips, NEOPIXEL_STRIP_ID_0, 0, 255, 0, 0);
+        dma_transfer_syn();
+        sl_sleeptimer_delay_millisecond(64);
+        neopixel_set_all(&strips, NEOPIXEL_STRIP_ID_0, 0, 0, 0, 0);
+        dma_transfer_syn();
+    }
+}
+
+static void patern_2(void) {
+    for (int i = 0; i < 8; i++)
+    {
+        sl_sleeptimer_delay_millisecond(64);
+        neopixel_set_all(&strips, NEOPIXEL_STRIP_ID_0, 255, 0, 0, 0);
+        dma_transfer_syn();
+        sl_sleeptimer_delay_millisecond(64);
+        neopixel_set_all(&strips, NEOPIXEL_STRIP_ID_0, 0, 0, 0, 0);
+        dma_transfer_syn();
+    }
+}
+
 void app_init(void)
 {
 
   dma_init_controller();
   neopixel_init(&strips);
 
-  neopixel_set_all(&strips, NEOPIXEL_STRIP_ID_0, 255, 0, 0, 0);
-
-  neopixel_update(&strips, TIMER_TopGet(TIMER0));
-
-  neopixel_init_dma_transfer(&strips, (void*)&TIMER0->CC[0].OCB);
+  delay_ms(10);
 
 }
 
@@ -48,5 +107,14 @@ void app_init(void)
  ******************************************************************************/
 void app_process_action(void)
 {
-
+  while (1)
+    {
+      patern_0();
+      sl_sleeptimer_delay_millisecond(1000);
+      patern_1();
+      sl_sleeptimer_delay_millisecond(1000);
+      patern_2();
+      sl_sleeptimer_delay_millisecond(1000);
+    }
 }
+
